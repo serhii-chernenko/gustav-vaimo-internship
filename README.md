f2accf1109d6dac476e3a1de03021dbcebd9ed3b
Serhii Chernenko <serhii.chernenko@vaimo.com>
2023-10-23 00:31:27 +0300

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# gustav-vaimo-internship
+CI/CD repo to deploy and run tests for a new theme of Magento 2
---
+++ registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/VaimoInternship/gustav', __DIR__);
